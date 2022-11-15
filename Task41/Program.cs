fb15659440e2749175822e228439101719042a75
lessya <lessya@yandex.ru>
2022-11-15 10:21:47 +0300

    задачи 41, 43

---
+++ Task41/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите размер массива M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+
+InputArray(array);
+PrintArray(array);
+Console.WriteLine($"Количество положительных элементов в массиве = {CountDigitPositive(array)} ");
+
+// Функция ввода массива
+void InputArray(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}й элемент массива: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+}
+
+//Функция вывода массива на экран
+void PrintArray(int[] arr)
+{
+
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i != arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+//Функция подсчета чисел больше нуля в массиве
+int CountDigitPositive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count = count + 1;
+    }
+    return count;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите b1: ");
+double numB1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double numK1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double numB2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double numK2 = Convert.ToInt32(Console.ReadLine());
+
+double numX = StraightLineCrossX(numK1, numK2, numB1, numB2);
+double numY = StraightLineCrossY(numK1, numB1, numX);
+Console.WriteLine($"Точка пересечения двух прямых ({numX};{numY})");
+
+//Функция нахождения точки пересечения двух прямых X
+double StraightLineCrossX(double k1, double k2, double b1, double b2)
+{
+double x = (b2-b1)/(k1-k2);
+return x;
+}
+
+//Функция нахождения точки пересечения двух прямых Y
+double StraightLineCrossY(double k1, double b1, double x)
+{
+double y = k1*x+b1;
+return y;
+}
