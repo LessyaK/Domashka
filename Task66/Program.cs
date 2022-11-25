f94ae4386d5413163f635a6091451152907c2f59
lessya <lessya@yandex.ru>
2022-11-25 15:33:18 +0300

    задачи 64, 66, 68

---
+++ Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m == n) Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N = 0");
+else
+{
+    if (m > n)
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+    GapNumberSum(m, n, 0);
+}
+
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
